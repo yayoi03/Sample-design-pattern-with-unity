d31773532d8ed5d158b0c34de89ebfe302e44c2e
yayoi03 <uni.ani367@gmail.com>
2020-02-29 19:14:48 +0900

    add Singleton pattern

---
+++ Assets/Scripts/Singleton/PlayerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start.");
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        
+        if (obj1 == obj2)
+        {
+            Debug.Log("obj1とobj2は同じインスタンスです。");
+        }
+        else
+        {
+            Debug.Log("obj1とobj2は同じインスタンスではありません。");
+        }
+        Debug.Log("End.");
+    }
+}
---
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton
+{
+    private static Singleton singleton = new Singleton();
+    private Singleton()
+    {
+        Debug.Log("インスタンスを生成しました。");
+    }
+    public static Singleton getInstance()
+    {
+        return singleton;
+    }
+}
+
